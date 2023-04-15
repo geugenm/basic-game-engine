fc190812d253788f1a435e7aecb02d8bf8b1df37
Eugene <glebajk@gmail.com>
2023-04-15 17:52:24 +0300

    Added CPM config + packages files init

--- modules/cfg/CPM.cmake/CPM.cmake.cmake
+++ modules/cfg/CPM.cmake/CPM.cmake.cmake
@@ -1,2 +1,11 @@
-project(CPM.cmake)
-add_subdirectory(../CPM.cmake)
+# Import the CPM.cmake module
+include(../../CPM.cmake/cmake/CPM.cmake)
+
+# Define the path to the packages directory
+set(CPM_PACKAGES_DIR packages)
+
+# Define the path to the source cache directory (Overriding the CPM variable)
+set(CPM_SOURCE_CACHE ../../../cache)
+
+# Set the option to use local packages to false
+set(CPM_USE_LOCAL_PACKAGES OFF)
