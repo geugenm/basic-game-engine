8c999d19b9729941fa3447773d53e2f288c86f9d
Eugene <glebajk@gmail.com>
2023-04-16 08:44:30 +0300

    Fixed global set from compile options

--- cmake/configure_compiler.cmake
+++ cmake/configure_compiler.cmake
@@ -1,4 +1,4 @@
-# Check for C++17 support
+# Check for C++23 support
 include(CheckCXXCompilerFlag)
 CHECK_CXX_COMPILER_FLAG("-std=c++23" COMPILER_SUPPORTS_CXX23)
 if (COMPILER_SUPPORTS_CXX23)
@@ -6,8 +6,3 @@ if (COMPILER_SUPPORTS_CXX23)
 else ()
     message(FATAL_ERROR "The compiler ${CMAKE_CXX_COMPILER} has no C++23 support. Please use a different C++ compiler.")
 endif ()
-
-# Set the C++ standard and other compiler flags
-set(CMAKE_CXX_STANDARD 23)
-set(CMAKE_CXX_STANDARD_REQUIRED ON)
-set(CMAKE_CXX_EXTENSIONS OFF)
