45e21b3d475a0dd1728baee960a22e0147dff32a
Eugene <glebajk@gmail.com>
2023-04-16 16:11:21 +0300

    Returned virtual nature of EngineFactory

--- include/engine/engine_factory.hxx
+++ include/engine/engine_factory.hxx
@@ -6,9 +6,9 @@ namespace Game {
     class EngineFactory {
     public:
         virtual ~EngineFactory() {}
-        std::shared_ptr<IModel> createModel();
-        std::shared_ptr<IPresenter> createPresenter();
-        std::shared_ptr<IView> createView();
+        virtual std::shared_ptr<IModel> createModel();
+        virtual std::shared_ptr<IPresenter> createPresenter();
+        virtual std::shared_ptr<IView> createView();
     };
 }
 
