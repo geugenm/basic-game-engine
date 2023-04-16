cefd2a58c42a7ff7c11c9418dbbb9fe7a830ec49
Eugene <glebajk@gmail.com>
2023-04-16 22:49:22 +0300

    Refactoring + reload removed

--- include/engine/misc/reload_attached_library.hxx
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "../engine.hxx"
-
-extern "C" void reloadView(const std::string& libraryName);
--- src/main.cxx
+++ src/main.cxx
@@ -1,27 +1,32 @@
 #include "engine/engine.hxx"
-#include "engine/misc/reload_attached_library.hxx"
 
 #include <chrono>
 #include <iostream>
 #include <thread>
 
 int main() {
-    Game::Engine::getInstance().getPresenter()->initialize();
-    auto start = std::chrono::steady_clock::now();
-    auto period = std::chrono::seconds(2);
-    while (true) {
-        Game::Engine::getInstance().getPresenter()->update();
-        Game::Engine::getInstance().getPresenter()->render();
-        auto now = std::chrono::steady_clock::now();
-        auto duration =
-            std::chrono::duration_cast<std::chrono::milliseconds>(now - start);
-        if (duration >= period) {
-            std::cout << "Passed 2 seconds!" << std::endl;
-            reloadView("1234.so");
-            start = now;
+    try {
+        auto& engine = Game::Engine::getInstance();
+        engine.getPresenter()->initialize();
+
+        const auto period = std::chrono::seconds(2);
+        auto nextFrameTime = std::chrono::steady_clock::now() + period;
+
+        while (true) {
+            engine.getPresenter()->update();
+            engine.getPresenter()->render();
+
+            const auto now = std::chrono::steady_clock::now();
+            if (now >= nextFrameTime) {
+                std::cout << "Passed 2 seconds!" << std::endl;
+                nextFrameTime += period;
+            }
+
+            std::this_thread::sleep_for(std::chrono::milliseconds(16));
         }
-        std::this_thread::sleep_for(
-            std::chrono::milliseconds(16)); // sleep for 1/60 seconds
+    } catch (const std::exception& ex) {
+        std::cerr << "Unhandled exception: " << ex.what() << std::endl;
+        return EXIT_FAILURE;
     }
 
     return EXIT_SUCCESS;
