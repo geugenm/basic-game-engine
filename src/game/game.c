6117d6993967b9799d08b765489592875b3c6c71
Eugene <glebajk@gmail.com>
2023-04-17 16:58:22 +0300

    Added game files

---
+++ src/game/game.c
@@ -0,0 +1,5 @@
+//
+// Created by geugenm on 4/17/23.
+//
+
+#include "game.h"
---
+++ src/game/game.h
@@ -0,0 +1,8 @@
+//
+// Created by geugenm on 4/17/23.
+//
+
+#ifndef BASIC_GAME_ENGINE_GAME_H
+#define BASIC_GAME_ENGINE_GAME_H
+
+#endif // BASIC_GAME_ENGINE_GAME_H
