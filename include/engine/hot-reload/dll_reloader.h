b2846e9e80880f99bb6911bf093d40e75b633e09
Eugene <glebajk@gmail.com>
2023-04-20 00:11:49 +0300

    Preambula to hot-reload

--- include/engine/hot-reload/dll_reloader.h
+++ include/engine/hot-reload/dll_reloader.h
@@ -8,8 +8,6 @@ struct IDLLReloader {
 
     virtual void set_new_library_path(const char* path) = 0;
 
-    virtual void set_current_library_handle(void*& handle) = 0;
-
     virtual void call_external_method_by_name(const char* method_name) = 0;
 
     virtual void reload_library() = 0;
@@ -18,6 +16,6 @@ struct IDLLReloader {
 };
 
 extern "C" IDLLReloader* create_dll_reloader();
-extern "C" void destroy_dll_reloader();
+extern "C" void destroy_dll_reloader(IDLLReloader * reloader);
 
 #endif // BASIC_GAME_ENGINE_RELOADER_H
--- src/sdl/dll_reloader.cxx
+++ src/sdl/dll_reloader.cxx
@@ -1,64 +1,84 @@
+#include "engine/hot-reload/dll_reloader.h"
 #include "apps/application.h"
 
 #include <SDL3/SDL.h>
 #include <filesystem>
 #include <iostream>
 
-class
+class SDLDLLReloader : public IDLLReloader {
+public:
+    SDLDLLReloader() { }
 
-    Application*
-    reloadApplicationLibrary(const Application* currentApplication,
-        void*& currentHandle)
-{
-    try {
-        if (currentApplication != nullptr) {
-            SDL_UnloadObject(currentHandle);
+    ~SDLDLLReloader() override = default;
+
+    void set_target_library_path(const char* path) override
+    {
+        if (std::filesystem::exists(path) == false) {
+            std::cerr << "Target library is not found";
+            return;
         }
+        target_library_path_ = path;
+    }
 
-        const std::filesystem::path tempLibraryPath = "apps/5678.so";
-        if (std::filesystem::exists(tempLibraryPath)) {
-            if (remove(tempLibraryPath) != 0) {
-                const std::filesystem::path currentPath = std::filesystem::current_path();
-                throw std::runtime_error(
-                    "Error: can't remove file: " + tempLibraryPath.string() + ", current path: " + currentPath.string());
-            }
+    void set_new_library_path(const char* path) override
+    {
+        if (std::filesystem::exists(path) == false) {
+            std::cerr << "New library is not found";
+            return;
         }
+        new_library_path_ = path;
+    }
 
-        const std::filesystem::path newLibraryPath = "apps/1234.so";
-        copy(newLibraryPath, tempLibraryPath);
+    void call_external_method_by_name(const char* method_name) override
+    {
+        get_sdl_function_pointer(method_name)();
+    }
 
-        void* gameHandle = SDL_LoadObject(tempLibraryPath.c_str());
+    void reload_library() override
+    {
+    }
+
+    void reset() override
+    {
+    }
+
+    [[nodiscard]] SDL_FunctionPointer get_sdl_function_pointer(const std::string_view& method_name)
+    {
+        void* gameHandle = SDL_LoadObject(new_library_path_.c_str());
 
         if (gameHandle == nullptr) {
-            throw std::runtime_error("Error: failed to load dynamic library: " + tempLibraryPath.string() + ", " + SDL_GetError());
+            throw std::runtime_error("Error: failed to load dynamic library: " + new_library_path_.string() + ", " + SDL_GetError());
         }
 
-        currentHandle = gameHandle;
-
-        SDL_FunctionPointer createGameFuncPtr = SDL_LoadFunction(gameHandle, "createApplication");
+        SDL_FunctionPointer createGameFuncPtr = SDL_LoadFunction(gameHandle, method_name.data());
 
         if (createGameFuncPtr == nullptr) {
             throw std::runtime_error(
                 "Error: failed to load function 'createApplication' from "
                 "dynamic library: "
-                + tempLibraryPath.string() + ", " + SDL_GetError());
+                + new_library_path_.string() + ", " + SDL_GetError());
         }
 
-        using create_game_ptr = decltype(&createApplication);
-
-        auto createGameFunc = reinterpret_cast<create_game_ptr>(createGameFuncPtr);
-
-        Application* newGame = createGameFunc();
-
-        if (newGame == nullptr) {
-            throw std::runtime_error(
-                "Error: function 'createApplication' returned null pointer");
-        }
-
-        return newGame;
-
-    } catch (const std::exception& e) {
-        std::cerr << e.what() << std::endl;
-        return nullptr;
+        return createGameFuncPtr;
     }
+
+private:
+    std::filesystem::path target_library_path_;
+
+    std::filesystem::path new_library_path_;
+
+    std::string_view method_name_;
+};
+
+IDLLReloader* create_dll_reloader()
+{
+    return new SDLDLLReloader();
+}
+
+void destroy_dll_reloader(IDLLReloader* reloader)
+{
+    if (reloader == nullptr) {
+        return;
+    }
+    delete reloader;
 }
