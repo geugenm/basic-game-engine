b3782cc64a57f45717c8f7e2c45a86df5b7951c7
Eugene <glebajk@gmail.com>
2023-04-23 22:07:45 +0300

    Added texture file

--- include/textures/texture_file.hxx
+++ include/textures/texture_file.hxx
@@ -3,23 +3,88 @@
 #include "abstract_file.hxx"
 #include "color.hxx"
 
+#include <fstream>
 #include <vector>
+#include <regex>
 
 class TextureFile : public File {
 public:
-    TextureFile(const std::filesystem::path & file_path) : File(file_path) {
+    TextureFile(const std::filesystem::path& file_path)
+        : File(file_path) {
         set_path(file_path);
     }
 
     void load() override {
+        std::ifstream in_file(get_path(), std::ios_base::binary);
+        in_file.exceptions(std::ios_base::failbit | std::ios_base::badbit);
 
+        // Read the PAM header from the input file
+        std::string header;
+        std::getline(in_file, header);
+        if (!std::regex_match(header, std::regex("^P7\\s+"))) {
+            throw std::invalid_argument("Error: Invalid PAM header.");
+        }
+
+        int new_width = 0;
+        int new_height = 0;
+
+        // Parse the PAM header fields
+        std::string field;
+        std::regex field_regex("^([A-Z]+)\\s+(.+)$");
+        while (std::getline(in_file, field) && field != "ENDHDR") {
+            std::smatch field_match;
+            if (std::regex_match(field, field_match, field_regex)) {
+                const std::string key = field_match[1];
+                const std::string value = field_match[2];
+                if (key == "WIDTH") {
+                    new_width = std::stoi(value);
+                } else if (key == "HEIGHT") {
+                    new_height = std::stoi(value);
+                }
+            }
+        }
+
+        // Check that we have read the width and height from the PAM header
+        if (new_width <= 0 || new_height <= 0) {
+            throw std::invalid_argument("Error: Invalid image dimensions.");
+        }
+
+        width_ = static_cast<size_t>(new_width);
+        height_ = static_cast<size_t>(new_height);
+
+        // Read the pixel data from the input file
+        const auto pixel_count = width_ * height_;
+        const auto buf_size = std::streamsize(sizeof(Color) * pixel_count);
+        in_file.read(reinterpret_cast<char*>(pixels_.data()), buf_size);
+
+        if (in_file.bad() == true) {
+            throw std::invalid_argument("Failed to read file " + get_path().string());
+        }
     }
 
-    void save(const std::filesystem::path & file_path) override {
-        std::ofstream out_file(file_path, std::ios::binary);
-    }
+    void save(const std::filesystem::path& file_path) override {
+        std::ofstream out_file(file_path, std::ios_base::binary);
+        out_file.exceptions(std::ios_base::failbit | std::ios_base::badbit);
 
+        out_file << "P7\n"
+                 << "WIDTH " << width_ << "\n"
+                 << "HEIGHT " << height_ << "\n"
+                 << "DEPTH 4\n"
+                 << "MAXVAL 255\n"
+                 << "TUPLTYPE RGB_ALPHA\n"
+                 << "ENDHDR\n";
+
+        const auto buffer_size =
+            std::streamsize(sizeof(Color) * pixels_.size());
+
+        const char* written_image_pixels =
+            reinterpret_cast<const char*>(pixels_.data());
+        out_file.write(written_image_pixels, buffer_size);
+    }
 
 private:
     std::vector<Color> pixels_;
+
+    std::size_t width_  = 0;
+    std::size_t height_ = 0;
 };
