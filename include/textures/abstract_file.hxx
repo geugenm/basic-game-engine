360eabe9d50e3f0890ac6635b8a762fb58a91074
Eugene <glebajk@gmail.com>
2023-04-23 21:45:07 +0300

    Renaming of arguments in abstract file

---
+++ include/textures/abstract_file.hxx
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <filesystem>
+
+class File {
+public:
+    File(const std::filesystem::path & file_path) {
+        set_path(file_path);
+    }
+
+    void set_path(const std::filesystem::path & file_path) {
+        if (exists(file_path) == false) {
+            throw std::invalid_argument("Given texture file path '" + file_path.string() + "' is not exist");
+        }
+        path_ = file_path;
+    }
+
+    [[nodiscard]] std::filesystem::path get_path() const {
+        return path_;
+    }
+
+    virtual void load() = 0;
+    virtual void save(const std::filesystem::path & path) = 0;
+
+private:
+    std::filesystem::path path_;
+};
