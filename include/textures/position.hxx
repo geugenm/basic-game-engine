83c4d9fc76bed2b5f728cf76bfaa1f6515d8ef1d
Eugene <glebajk@gmail.com>
2023-04-23 22:14:59 +0300

    Position renaming

--- include/textures/position.hxx
+++ include/textures/position.hxx
@@ -3,33 +3,33 @@
 #include <cmath>
 #include <random>
 
-struct position {
+struct Position {
     int32_t x = 0;
     int32_t y = 0;
 
-    constexpr position() = default;
+    constexpr Position() = default;
 
-    constexpr position(int32_t x_, int32_t y_)
+    constexpr Position(int32_t x_, int32_t y_)
         : x(x_)
         , y(y_) { }
 
     constexpr double length() const { return std::sqrt(x * x + y * y); }
 
-    friend constexpr position operator-(const position& left,
-                                        const position& right) {
-        return position(left.x - right.x, left.y - right.y);
+    friend constexpr Position operator-(const Position& left,
+                                        const Position& right) {
+        return Position(left.x - right.x, left.y - right.y);
     }
 
-    friend constexpr bool operator==(const position& left,
-                                     const position& right) {
+    friend constexpr bool operator==(const Position& left,
+                                     const Position& right) {
         return left.x == right.x && left.y == right.y;
     }
 
-    static position generate_random(const int & width, const int & height) {
+    static Position generate_random(const int & width, const int & height) {
         static std::random_device              rd;
         static std::mt19937                    gen(rd());
         std::uniform_int_distribution<int32_t> dist_x(0, width - 1);
         std::uniform_int_distribution<int32_t> dist_y(0, height - 1);
-        return position(dist_x(gen), dist_y(gen));
+        return Position(dist_x(gen), dist_y(gen));
     }
 };
