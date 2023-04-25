f2c67bd2e857cb354b06936c98cd924b32f89a42
Eugene <glebajk@gmail.com>
2023-04-26 00:15:45 +0300

    Dots are visible~

--- include/render/shapes/abstract_shape.hxx
+++ include/render/shapes/abstract_shape.hxx
@@ -40,29 +40,51 @@ struct Position2DHash {
 
 
 
-class IndexedShape {
+class IndexedShape
+{
   public:
     IndexedShape() = default;
 
-    void add_2d_shape(const Shape2D& shape) {
-        for (const auto& vertex : shape.get_vertices()) {
-            auto it = vertices_.find(vertex);
-            if (it == vertices_.end()) {
-                vertices_[vertex] = vertex_count_;
-                vertex_count_++;
+    void add_2d_shape(const Shape2D& shape)
+    {
+        // Get the shape's vertices
+        const auto vertices = shape.get_vertices();
+
+        // Loop through the vertices
+        for (const auto& vertex : vertices)
+        {
+            // Check if the vertex has already been drawn
+            auto iter = vertices_.find(vertex);
+            if (iter == vertices_.end() || !iter->second.is_drawn)
+            {
+                // Vertex has not been drawn, add it to the index
+                vertices_[vertex] = { vertices_.size(), false };
             }
         }
     }
 
-    void add_vertex(const Position2D& vertex) {
-        auto it = vertices_.find(vertex);
-        if (it == vertices_.end()) {
-            vertices_[vertex] = vertex_count_;
-            vertex_count_++;
+    void draw_on(Texture& texture, const ColorRGB& color)
+    {
+        // Loop through the vertices
+        for (auto& [vertex, data] : vertices_)
+        {
+            // Check if the vertex has been drawn
+            if (!data.is_drawn)
+            {
+                // Vertex has not been drawn, draw it and mark it as drawn
+                texture.set_pixel(vertex, color);
+                data.is_drawn = true;
+            }
         }
     }
 
   private:
-    std::unordered_map<Position2D, size_t, Position2DHash> vertices_;
-    size_t vertex_count_ = 0;
+    struct VertexData
+    {
+        size_t index;
+        bool is_drawn;
+    };
+
+    std::unordered_map<Position2D, VertexData, Position2DHash> vertices_;
 };
+
--- test/draw_polygon.cxx
+++ test/draw_polygon.cxx
@@ -38,6 +38,36 @@ TEST(Polygon2DTest, DrawMultiplePolygonsTest)
     handler.save();
 }
 
+TEST(Polygon2DTest, DrawIndexedShape)
+{
+    const Position2D start = Position2D::generate_random(200, 230);
+    const Position2D end = Position2D::generate_random(300, 1200);
+
+    IndexedShape shape;
+
+    Texture texture;
+    texture.set_shape(BoundingBox(start, end));
+
+    Polygon2D polygon(start, end, 3);
+
+    polygon.add_vertex({0, 0});
+    polygon.add_vertex({200, 200});
+    //polygon.draw_on(texture, ColorRGB{0, 0, 255});
+
+    shape.add_2d_shape(polygon);
+
+    const Position2D next = Position2D::generate_random(200, 230);
+    Polygon2D polygon2(next, end, 3);
+
+    shape.add_2d_shape(polygon);
+
+    shape.draw_on(texture, {0, 255, 255});
+
+    std::filesystem::path path("test_polygon_additional_vertex_indexed.ppm");
+    PpmHandler handler(path, texture);
+    handler.save();
+}
+
 auto main(int argc, char **argv) -> int
 {
     ::testing::InitGoogleTest(&argc, argv);
