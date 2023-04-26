79fdb6968f8b026274186d1d02c71b2b072090f4
Eugene <glebajk@gmail.com>
2023-04-26 23:09:43 +0300

    For preview added sleep

--- test/draw_line.cxx
+++ test/draw_line.cxx
@@ -1,20 +1,24 @@
 #include "render/shapes/line_2d.hxx"
 #include "render/textures/ppm_handler.hxx"
+
+#include <Tracy/tracy/Tracy.hpp>
 #include <gtest/gtest.h>
+#include <unistd.h>
 
 TEST(Line2DTest, DrawRandomLinesTest)
 {
-    constexpr int num_lines = 100;
+    constexpr int num_lines = 500;
 
-    const Position2D start = Position2D::generate_random(200, 230);
-    const Position2D end = Position2D::generate_random(300, 1200);
+    const Position2D start = Position2D::generate_random(100, 730);
+    const Position2D end = Position2D::generate_random(500, 1900);
 
     Line2D line(start, end);
     Texture texture;
-    texture.set_shape({400, 1300});
+    texture.set_shape({2000, 2000});
 
     for (int i = 0; i < num_lines; ++i)
     {
+        sleep(1);
         line.draw_random(texture);
     }
 
