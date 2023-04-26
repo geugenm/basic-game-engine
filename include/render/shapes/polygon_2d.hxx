b6d6e5e79a0b3eb8c961364f592e7c942fdb93e4
Eugene <glebajk@gmail.com>
2023-04-26 19:21:41 +0300

    Added constructor for Polygon2D

--- include/render/shapes/polygon_2d.hxx
+++ include/render/shapes/polygon_2d.hxx
@@ -10,30 +10,34 @@ class Polygon2D final : public Shape2D
 {
   public:
     explicit Polygon2D(BoundingBox box, const size_t &sides_amount)
-        : bounding_box_(std::move(box)), sides_amount_(sides_amount)
+        : bounding_box_(std::move(box))
     {
-        init();
+        init(sides_amount);
     }
 
-    Polygon2D(const Position2D &start, const Position2D &end, const size_t &sides_amount) : sides_amount_(sides_amount)
+    explicit Polygon2D(BoundingBox box, Vertices vertices) : bounding_box_(std::move(box)), vertices_(std::move(vertices)) {
+        
+    }
+
+    Polygon2D(const Position2D &start, const Position2D &end, const size_t &sides_amount)
     {
         bounding_box_ = BoundingBox(start, end);
-        init();
+        init(sides_amount);
     }
 
     Polygon2D(const Polygon2D &other) : Shape2D(other)
     {
-        init();
+        init(other.get_vertices().size());
     }
 
     ~Polygon2D() override = default;
 
     void draw_on(Texture &texture, const ColorRGB &color) const override
     {
-        for (size_t i = 0; i < sides_amount_; ++i)
+        for (size_t i = 0; i < vertices_.size(); ++i)
         {
             const Position2D &p1 = vertices_[i];
-            const Position2D &p2 = vertices_[(i + 1) % sides_amount_];
+            const Position2D &p2 = vertices_[(i + 1) % vertices_.size()];
             Line2D l(p1, p2);
             l.draw_on(texture, color);
         }
@@ -63,8 +67,7 @@ class Polygon2D final : public Shape2D
                 return;
             }
         }
-        sides_amount_++;
-        vertices_.reserve(sides_amount_);
+        vertices_.reserve(vertices_.size() + 1);
         vertices_.push_back(position);
     }
 
@@ -73,11 +76,11 @@ class Polygon2D final : public Shape2D
     }
 
   private:
-    void init()
+    void init(const size_t & sides_amount)
     {
-        angle_ = 2.0 * M_PI / static_cast<double>(sides_amount_);
+        double angle = 2.0 * M_PI / static_cast<double>(sides_amount);
 
-        vertices_.resize(sides_amount_);
+        vertices_.resize(sides_amount);
         int32_t radius_x = static_cast<int32_t>(bounding_box_.width) / 2;
         int32_t radius_y = static_cast<int32_t>(bounding_box_.height) / 2;
 
@@ -91,22 +94,18 @@ class Polygon2D final : public Shape2D
             radius_y -= 1;
         }
 
-        center_ = {radius_x, radius_y};
+        Position2D center = {radius_x, radius_y};
 
-        for (size_t i = 0; i < sides_amount_; ++i)
+        for (size_t i = 0; i < sides_amount; ++i)
         {
-            const double theta = angle_ * static_cast<double>(i);
-            const double x = center_.x + radius_x * std::cos(theta);
-            const double y = center_.y + radius_y * std::sin(theta);
+            const double theta = angle * static_cast<double>(i);
+            const double x = center.x + radius_x * std::cos(theta);
+            const double y = center.y + radius_y * std::sin(theta);
             vertices_[i] = {static_cast<int>(x), static_cast<int>(y)};
         }
     }
 
     BoundingBox bounding_box_;
 
-    size_t sides_amount_;
-    double angle_;
-
     Vertices vertices_;
-    Position2D center_;
 };
