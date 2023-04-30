0c57f4ef7269a01cbcff39b11d99e8047c836184
Eugene <glebajk@gmail.com>
2023-04-30 22:54:17 +0300

    shrink_bounding_box added

--- include/render/shapes/abstract_shape.hxx
+++ include/render/shapes/abstract_shape.hxx
@@ -18,20 +18,24 @@ class Shape2D
 
     virtual void draw_on(Texture &texture, const ColorRGB &color) = 0;
 
-    [[nodiscard]] virtual std::string string() const {
+    [[nodiscard]] virtual std::string string() const
+    {
         std::string result = "Shape2D: (" + std::to_string(vertices_.size()) + ") vertices\n";
-        for (std::size_t i = 0; i < vertices_.size(); ++i) {
-            const auto& vertex = vertices_[i];
+        for (std::size_t i = 0; i < vertices_.size(); ++i)
+        {
+            const auto &vertex = vertices_[i];
             result += "Vertex " + std::to_string(i) + ": " + vertex.string() + "\n";
         }
         return result;
     }
 
-    [[nodiscard]] const Vertices & get_vertices() const {
+    [[nodiscard]] const Vertices &get_vertices() const
+    {
         return vertices_;
     }
 
-    [[nodiscard]] const BoundingBox & get_bounding_box() const {
+    [[nodiscard]] const BoundingBox &get_bounding_box() const
+    {
         return bounding_box_;
     }
 
@@ -52,14 +56,31 @@ class Shape2D
 
     void set_vertices(const Vertices &vertices)
     {
-        vertices_ = vertices;
+        access_vertices() = vertices;
     }
 
-    [[nodiscard]] Vertices & access_vertices() {
+    void shrink_bounding_box() {
+        int min_x = INT_MAX, min_y = INT_MAX, max_x = INT_MIN, max_y = INT_MIN;
+        for (const auto &vertex : access_vertices())
+        {
+            min_x = std::min(min_x, vertex.x);
+            min_y = std::min(min_y, vertex.y);
+            max_x = std::max(max_x, vertex.x);
+            max_y = std::max(max_y, vertex.y);
+        }
+
+        const Position2D bottom_left = {min_x, min_y};
+        const Position2D top_right = {max_x, max_y};
+        set_bounding_box(BoundingBox(bottom_left, top_right));
+    }
+
+    [[nodiscard]] Vertices &access_vertices()
+    {
         return vertices_;
     }
 
-    [[nodiscard]] BoundingBox & access_bounding_box() {
+    [[nodiscard]] BoundingBox &access_bounding_box()
+    {
         return bounding_box_;
     }
 
