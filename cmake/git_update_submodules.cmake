e17ecb6c75c7e220440545f0b03ae48fe75b3d1e
Eugene <glebajk@gmail.com>
2023-05-02 21:57:24 +0300

    Added automatic submodule init from cmake

---
+++ cmake/git_update_submodules.cmake
@@ -0,0 +1,39 @@
+function(update_submodules)
+  set(GIT_REPO_PATH "${CMAKE_CURRENT_LIST_DIR}/../")
+
+  # Check if Git is installed
+  find_package(Git REQUIRED)
+
+  # Check if the Git repository exists
+  if (NOT EXISTS ${GIT_REPO_PATH}/.git)
+    message(FATAL_ERROR "Git repository not found at ${GIT_REPO_PATH}!")
+  endif ()
+
+  # Update submodules
+  execute_process(
+          COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive
+          WORKING_DIRECTORY ${GIT_REPO_PATH}
+          RESULT_VARIABLE submodule_result
+          OUTPUT_VARIABLE submodule_output
+  )
+
+  # Check if the submodule update was successful
+  if (NOT submodule_result EQUAL 0)
+    message(FATAL_ERROR "Failed to update submodules in Git repository at ${GIT_REPO_PATH}!")
+  else ()
+    message(STATUS "Submodules updated successfully.")
+  endif ()
+
+  # Check if the submodule directories exist
+  set(submodules modules/CPM.cmake)
+  foreach(submodule ${submodules})
+    if(NOT EXISTS ${GIT_REPO_PATH}/${submodule})
+      message(FATAL_ERROR "Submodule ${submodule} not found in Git repository at ${GIT_REPO_PATH}!")
+    endif()
+  endforeach()
+
+  # Output submodule update output
+  message(STATUS "Git submodule update output: ${submodule_output}")
+endfunction()
+
+update_submodules()
--- modules/cfg/CPM.cmake/CPM.cmake.cmake
+++ modules/cfg/CPM.cmake/CPM.cmake.cmake
@@ -1,7 +1,16 @@
-include(${CMAKE_CURRENT_LIST_DIR}/../../CPM.cmake/cmake/CPM.cmake)
+if (EXISTS "${CMAKE_CURRENT_LIST_DIR}/../../CPM.cmake/cmake/CPM.cmake")
+  include("${CMAKE_CURRENT_LIST_DIR}/../../CPM.cmake/cmake/CPM.cmake")
+else ()
+  message(FATAL_ERROR "CPM.cmake not found! Make sure you've initialized git submodules or download CPM.cmake repo and put it in the 'modules' directory of your project.")
+endif ()
 
-set(CPM_PACKAGES_DIR ${CMAKE_CURRENT_LIST_DIR}/packages)
+set(CPM_PACKAGES_DIR "${CMAKE_CURRENT_LIST_DIR}/packages")
 
-set(CPM_SOURCE_CACHE ${CMAKE_CURRENT_LIST_DIR}/../../../cache)
+set(CPM_SOURCE_CACHE "${CMAKE_CURRENT_LIST_DIR}/../../../cache")
 
-set(CPM_USE_LOCAL_PACKAGES OFF)
+set(CPM_USE_LOCAL_PACKAGES ON)
+
+message(STATUS "Using CPM.cmake version ${CPM_VERSION}")
+message(STATUS "CPM packages directory: ${CPM_PACKAGES_DIR}")
+message(STATUS "CPM source cache directory: ${CPM_SOURCE_CACHE}")
+message(STATUS "Using local packages: ${CPM_USE_LOCAL_PACKAGES}")
