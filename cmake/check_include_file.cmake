438531668f8a3536011421347c351050a863e356
Eugene <glebajk@gmail.com>
2023-05-02 22:48:09 +0300

    Added basic headers checker

---
+++ cmake/check_include_file.cmake
@@ -0,0 +1,11 @@
+include(CheckIncludeFile)
+
+check_include_file("stdio.h" HAVE_STDIO_H)
+check_include_file("stdlib.h" HAVE_STDLIB_H)
+
+# Add status message for include check result
+if(HAVE_STDIO_H AND HAVE_STDLIB_H)
+  message(STATUS "Required headers found.")
+else()
+  message(FATAL_ERROR "Basic required headers(stdio.h and stdlib.h) are not found.")
+endif()
--- scripts/add_submodule.sh
+++ scripts/add_submodule.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
+
 SCRIPT_DIR="$( cd "$( dirname "\${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 echo "Enter Git submodule URL:"
