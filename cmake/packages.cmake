84ebf2e2ff9ceec0bfe9f63804db555e09fc1f35
Eugene <glebajk@gmail.com>
2023-05-05 23:03:23 +0300

    Refactored cmake message to print package status

--- cmake/packages.cmake
+++ cmake/packages.cmake
@@ -1,10 +1,14 @@
 include(${CMAKE_CURRENT_LIST_DIR}/../modules/cfg/CPM.cmake/CPM.cmake.cmake)
 
-include(${CPM_PACKAGES_DIR}/gtest.cmake)
-include(${CPM_PACKAGES_DIR}/sdl.cmake)
+file(GLOB CPM_PACKAGE_FILES "${CPM_PACKAGES_DIR}/*.cmake")
 
-include(${CPM_PACKAGES_DIR}/sdl.cmake)
+foreach (CPM_PACKAGE_FILE IN LISTS CPM_PACKAGE_FILES)
+  include(${CPM_PACKAGE_FILE})
+  get_filename_component(PACKAGE_NAME ${CPM_PACKAGE_FILE} NAME_WE)
+  message(STATUS "Included package: ${PACKAGE_NAME}")
+endforeach ()
 
-include(${CPM_PACKAGES_DIR}/glog.cmake)
-include(${CPM_PACKAGES_DIR}/tracy.cmake)
-include(${CPM_PACKAGES_DIR}/ccache.cmake)
+message(STATUS "Using CPM.cmake version ${CPM_VERSION}")
+message(STATUS "CPM packages directory: ${CPM_PACKAGES_DIR}")
+message(STATUS "CPM source cache directory: ${CPM_SOURCE_CACHE}")
+message(STATUS "Using local packages: ${CPM_USE_LOCAL_PACKAGES}")
--- modules/cfg/CPM.cmake/CPM.cmake.cmake
+++ modules/cfg/CPM.cmake/CPM.cmake.cmake
@@ -9,8 +9,3 @@ set(CPM_PACKAGES_DIR "${CMAKE_CURRENT_LIST_DIR}/packages")
 set(CPM_SOURCE_CACHE "${CMAKE_CURRENT_LIST_DIR}/../../../cache")
 
 set(CPM_USE_LOCAL_PACKAGES OFF)
-
-message(STATUS "Using CPM.cmake version ${CPM_VERSION}")
-message(STATUS "CPM packages directory: ${CPM_PACKAGES_DIR}")
-message(STATUS "CPM source cache directory: ${CPM_SOURCE_CACHE}")
-message(STATUS "Using local packages: ${CPM_USE_LOCAL_PACKAGES}")
