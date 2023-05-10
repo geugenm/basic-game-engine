d1e00007ce2221128dcbaf76f31b4bd43f186c60
Eugene <glebajk@gmail.com>
2023-05-10 20:13:47 +0300

    Use Vertex Buffer Objects (VBO) with the GL_STREAM_DRAW

--- test/sdl_engine_implementation.cxx
+++ test/sdl_engine_implementation.cxx
@@ -116,7 +116,7 @@ TEST(ShaderTest, ShaderOutput)
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STREAM_DRAW);
 
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
