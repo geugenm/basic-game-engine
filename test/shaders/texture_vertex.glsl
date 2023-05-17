6c0d8db38fce544b486d0eb3864050638b45672f
Eugene <glebajk@gmail.com>
2023-05-18 00:21:04 +0300

    Fixed scale problem

--- test/shaders/texture_vertex.glsl
+++ test/shaders/texture_vertex.glsl
@@ -12,5 +12,5 @@ void main()
 {
     gl_Position = transform * vec4(position, 1.0f);
     ourColor = color;
-    TexCoord = vec2(texCoord.x, 1.0 - texCoord.y);
+    TexCoord = texCoord;
 }
--- test/test_opengl_texture.cxx
+++ test/test_opengl_texture.cxx
@@ -1,84 +1,157 @@
-#include "render/picopng.hxx"
-#include "sdl_engine.hxx"
-
-#include <gtest/gtest.h>
+#include <sdl_engine.hxx>
 
 #include <complex>
-#include <fstream>
+#include <gtest/gtest.h>
 
-class TextureLoaderTest : public ::testing::Test {
-protected:
-    void SetUp() override {
-        SDL::Engine::instance().initialize("Test texture", 1280, 720);
-    }
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
-    void TearDown() override {
-        SDL::Engine::instance().destroy();
-    }
+TEST(SDLEngineTest, Init)
+{
+    const char* window_title        = "TestSDLEngine";
+    constexpr int32_t window_height = 1000;
+    constexpr int32_t window_width  = 1000;
+    SDL::Engine::instance().initialize(window_title, window_height, window_width);
 
-    SDL_Window* window = nullptr;
-    SDL_GLContext gl_context = nullptr;
-};
+    SDL::SomeShader shader;
+    shader.initialize_impl("shaders/texture_vertex.glsl", "shaders/texture_fragment.glsl");
 
-TEST_F(TextureLoaderTest, LoadTexture) {
+    // Set up vertex data (and buffer(s)) and attribute pointers
+    GLfloat vertices[] = {
+        // Positions          // Colors           // Texture Coords
+        0.5f,  0.5f,  0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, // Top Right
+        0.5f,  -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // Bottom Right
+        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // Bottom Left
+        -0.5f, 0.5f,  0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f  // Top Left
+    };
+    GLuint indices[] = {
+        // Note that we start from 0!
+        0, 1, 3, // First Triangle
+        1, 2, 3  // Second Triangle
+    };
+    GLuint VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    GL::listen_opengl_errors();
+
+    glGenBuffers(1, &VBO);
+    GL::listen_opengl_errors();
+
+    glGenBuffers(1, &EBO);
+    GL::listen_opengl_errors();
+
+    glBindVertexArray(VAO);
+    GL::listen_opengl_errors();
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    GL::listen_opengl_errors();
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    GL::listen_opengl_errors();
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    GL::listen_opengl_errors();
+
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    GL::listen_opengl_errors();
+
+    // Position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)0);
+    GL::listen_opengl_errors();
+
+    glEnableVertexAttribArray(0);
+    GL::listen_opengl_errors();
+
+    // Color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat),
+                          (GLvoid*)(3 * sizeof(GLfloat)));
+    GL::listen_opengl_errors();
+
+    glEnableVertexAttribArray(1);
+    GL::listen_opengl_errors();
+
+    // TexCoord attribute
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat),
+                          (GLvoid*)(6 * sizeof(GLfloat)));
+    GL::listen_opengl_errors();
+
+    glEnableVertexAttribArray(2);
+    GL::listen_opengl_errors();
+
+    glBindVertexArray(0); // Unbind VAO
+    GL::listen_opengl_errors();
+
     std::ifstream file("textures/texture.png", std::ios::binary | std::ios::ate);
-    if (!file.is_open()) {
-        FAIL() << "Failed to open PNG image";
+    if (!file.is_open())
+    {
+        throw std::invalid_argument("Failed to open PNG image");
     }
 
     std::streamsize size = file.tellg();
     file.seekg(0, std::ios::beg);
 
     std::vector<unsigned char> buffer(size);
-    if (!file.read(reinterpret_cast<char*>(buffer.data()), size)) {
-        FAIL() << "Failed to read PNG image";
+    if (!file.read(reinterpret_cast<char*>(buffer.data()), size))
+    {
+        throw std::runtime_error("Failed to read PNG image");
     }
 
     std::vector<unsigned char> png_data;
     unsigned long width, height;
 
     // Decode PNG data using picopng
-    unsigned error = decodePNG(png_data, width, height, buffer.data(), buffer.size());
-    if (error != 0) {
-        FAIL() << "Failed to decode PNG image";
+    auto error =
+        static_cast<unsigned int>(decodePNG(png_data, width, height, buffer.data(), buffer.size()));
+    if (error != 0)
+    {
+        throw std::runtime_error("Failed to decode PNG image");
     }
 
-    // Create texture from PNG data
+    if (png_data.empty())
+    {
+        throw std::runtime_error("Empty png result");
+    }
+
+    // Load and create a texture
     GLuint texture;
     glGenTextures(1, &texture);
-    glBindTexture(GL_TEXTURE_2D, texture);
+    GL::listen_opengl_errors();
 
+    glBindTexture(
+        GL_TEXTURE_2D,
+        texture); // All upcoming GL_TEXTURE_2D operations now have effect on this texture object
+    GL::listen_opengl_errors();
+
+    // Set texture filtering parameters
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    GL::listen_opengl_errors();
+
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    GL::listen_opengl_errors();
 
-    GLenum format = GL_RGBA;
-    glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, png_data.data());
+    // Load image, create texture and generate mipmaps
 
-    glBindTexture(GL_TEXTURE_2D, 0);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE,
+                 png_data.data());
+    GL::listen_opengl_errors();
 
-    // Check if texture was created successfully
-    EXPECT_NE(texture, 0u);
+    glBindTexture(GL_TEXTURE_2D,
+                  0); // Unbind texture when done, so we won't accidentily mess up our texture.
+    GL::listen_opengl_errors();
 
-    GLfloat vertices[] = {
-        // Позиции          // Цвета             // Текстурные координаты
-        0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // Верхний правый
-        0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // Нижний правый
-        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // Нижний левый
-        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // Верхний левый
-    };
 
-    glVertexAttribPointer(2, 2, GL_FLOAT,GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
-    glEnableVertexAttribArray(2);
 
-    GLuint VAO;
-    glBindTexture(GL_TEXTURE_2D, texture);
-    glBindVertexArray(VAO);
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-    glBindVertexArray(0);
+
+    auto transformLoc =
+        static_cast<GLint>(glGetUniformLocation(shader.get_shader_program_id(), "transform"));
+
+
 
     SDL_Event event;
     while (true)
     {
+        glViewport(0, 0, window_width, window_height);
+
         while (SDL_PollEvent(&event))
         {
             if (event.type == SDL_EVENT_QUIT)
@@ -87,12 +160,56 @@ TEST_F(TextureLoaderTest, LoadTexture) {
             }
         }
 
+        glActiveTexture(GL_TEXTURE0);
+        GL::listen_opengl_errors();
+
+        glBindTexture(GL_TEXTURE_2D, texture);
+        GL::listen_opengl_errors();
+
+        GLint textureUniformLocation =
+            glGetUniformLocation(shader.get_shader_program_id(), "ourTexture");
+        GL::listen_opengl_errors();
+
+        glUniform1i(textureUniformLocation, 0); // Set the texture uniform to use texture unit 0
+        GL::listen_opengl_errors();
+
+        shader.render();
+
+
+
+        glm::mat4 transform = glm::mat4(1.0f);
+        transform = glm::scale(transform, glm::vec3(0.2f, 0.2f, 0.2f));
+
+        // Get matrix's uniform location and set matrix
+        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
+        GL::listen_opengl_errors();
+
+
+
+
+        glBindVertexArray(VAO);
+        GL::listen_opengl_errors();
+
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        GL::listen_opengl_errors();
+
+        glBindVertexArray(0);
+        GL::listen_opengl_errors();
+
         SDL::Engine::Instance::instance().render();
     }
 
-    cleanup:
-    // Clean up
-    glDeleteTextures(1, &texture);
+cleanup:
+    glDeleteVertexArrays(1, &VAO);
+    GL::listen_opengl_errors();
+
+    glDeleteBuffers(1, &VBO);
+    GL::listen_opengl_errors();
+
+    glDeleteBuffers(1, &EBO);
+    GL::listen_opengl_errors();
+
+    SDL::Engine::instance().destroy();
 }
 
 auto main(int argc, char** argv) -> int
