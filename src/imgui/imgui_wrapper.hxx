7b95111bf1b473ba566aa6f4c141426bda94492d
Eugene <glebajk@gmail.com>
2023-05-27 21:20:54 +0300

    Added custom imgui style

--- src/imgui/imgui_wrapper.hxx
+++ src/imgui/imgui_wrapper.hxx
@@ -8,10 +8,46 @@
 
 namespace ImWrapper
 {
+
+void setup_style(bool is_dark, float alpha)
+{
+    ImGuiStyle &style = ImGui::GetStyle();
+
+    if (is_dark)
+    {
+        ImGui::StyleColorsDark();
+    }
+    else
+    {
+        ImGui::StyleColorsLight();
+    }
+
+    for (int i = 0; i <= ImGuiCol_COUNT; i++)
+    {
+        auto ei     = static_cast<ImGuiCol_>(i);
+        ImVec4 &col = style.Colors[i];
+        if ((ImGuiCol_ModalWindowDimBg != ei) && (ImGuiCol_NavWindowingDimBg != ei) &&
+            (col.w < 1.00f || (ImGuiCol_FrameBg == ei) || (ImGuiCol_WindowBg == ei) || (ImGuiCol_ChildBg == ei)))
+        {
+            col.w = alpha * col.w;
+        }
+    }
+
+    style.ChildBorderSize  = 1.0f;
+    style.FrameBorderSize  = 0.0f;
+    style.PopupBorderSize  = 1.0f;
+    style.WindowBorderSize = 0.0f;
+    style.WindowRounding   = 6.0f;
+    style.FrameRounding    = 3.0f;
+    style.Alpha            = 1.0f;
+}
+
 void init_imgui(SDL_Window *window, SDL_GLContext gl_context)
 {
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
+
+    setup_style(true, 0.8);
 
     ImGui_ImplSDL3_InitForOpenGL(window, gl_context);
     ImGui_ImplOpenGL3_Init("#version 330 core");
@@ -36,8 +72,10 @@ void process_event(const SDL_Event &event)
     ImGui_ImplSDL3_ProcessEvent(&event);
 }
 
-void render() {
+void render()
+{
     ImGui::Render();
     ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 }
-} // namespace ImUI
+
+} // namespace ImWrapper
