d28bfd204bf766e02103a11175364521058f74f2
Eugene <glebajk@gmail.com>
2023-06-05 20:04:06 +0300

    Small sound fixes

--- test/test_sdl_sound.cxx
+++ test/test_sdl_sound.cxx
@@ -1,4 +1,5 @@
 #include "sdl_sound.hxx"
+#include "opengl_functions.hxx"
 
 #include <SDL3/SDL.h>
 #include <cmath>
@@ -6,14 +7,17 @@
 
 TEST(SDL_Audio, test_audio_mixer_class)
 {
-    auto mixer = new sdl_subsdk::audio_mixer("wav/car_on.WAV");
+    auto mixer = new sdl_subsdk::audio_mixer("wav/machine_gun.WAV");
     mixer->initialize();
+
+    auto sound2 = new sdl_subsdk::audio_mixer("wav/car_on.WAV");
+    sound2->initialize();
 
     auto *engine =
         new sdl_subsdk::engine("OpenGL 3.0 SDL Sound Test", 1280, 720);
 
     engine->initialize();
-    //opengl_subsdk::enable_debug_mode();
+    opengl_subsdk::enable_debug_mode();
 
     bool running = true;
     SDL_Event event;
