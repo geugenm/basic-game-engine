0f51af4f9c448d4a4b16e884e8086c8f5f48d695
Eugene <glebajk@gmail.com>
2023-06-06 18:08:04 +0300

    Texture is now has a collision

--- test/test_opengl_texture.cxx
+++ test/test_opengl_texture.cxx
@@ -12,9 +12,9 @@
 
 TEST(SDLEngineTest, Init)
 {
-    static const char *window_title        = "TestSDLEngine";
-    constexpr int32_t window_height = 1000;
-    constexpr int32_t window_width  = 1000;
+    static const char *window_title = "TestSDLEngine";
+    constexpr int32_t window_height = 800;
+    constexpr int32_t window_width  = 400;
 
     auto engine =
         new sdl_subsdk::engine(window_title, window_height, window_width);
@@ -31,39 +31,49 @@ TEST(SDLEngineTest, Init)
     glm::vec2 position(0.5f, -0.5f);
     float rotationAngle = 0.0f;
 
-    float moveSpeed = 0.01f;
+    float moveSpeed   = 0.01f;
     float rotateSpeed = 0.01f;
-
 
     SDL_Event event;
     while (true)
     {
         glViewport(0, 0, window_width, window_height);
-        
+
         while (SDL_PollEvent(&event))
         {
             if (event.type == SDL_EVENT_KEY_DOWN)
             {
+                float halfWidth = 0.6f * 0.5f;
+                float halfHeight = 0.6f * 0.5f;
+
                 switch (event.key.keysym.sym)
                 {
                     case SDLK_w:
                         position.x += moveSpeed * cos(rotationAngle);
                         position.y += moveSpeed * sin(rotationAngle);
+                        position.x = std::clamp(position.x, -1.0f + halfWidth, 1.0f - halfWidth);
+                        position.y = std::clamp(position.y, -1.0f + halfHeight, 1.0f - halfHeight);
                         break;
                     case SDLK_s:
                         position.x -= moveSpeed * cos(rotationAngle);
                         position.y -= moveSpeed * sin(rotationAngle);
+                        position.x = std::clamp(position.x, -1.0f + halfWidth, 1.0f - halfWidth);
+                        position.y = std::clamp(position.y, -1.0f + halfHeight, 1.0f - halfHeight);
                         break;
-                    case SDLK_a: rotationAngle += rotateSpeed; break;
-                    case SDLK_d: rotationAngle -= rotateSpeed; break;
+                    case SDLK_a:
+                        rotationAngle += rotateSpeed;
+                        break;
+                    case SDLK_d:
+                        rotationAngle -= rotateSpeed;
+                        break;
                 }
             }
 
-            if (event.type == SDL_EVENT_QUIT) {
+            if (event.type == SDL_EVENT_QUIT)
+            {
                 goto cleanup;
             }
         }
-
 
         shader.use();
 
@@ -73,7 +83,7 @@ TEST(SDLEngineTest, Init)
         glUniform1i(textureUniformLocation,
                     0); // Set the texture uniform to use texture unit 0
 
-        constexpr float aspectRatio = 1.0f;
+        constexpr float aspectRatio = static_cast<float>(window_width) / static_cast<float>(window_height);
         glm::mat4 aspectMatrix = glm::scale(glm::mat4(1.0f), glm::vec3(aspectRatio, 1.0f, 1.0f));
 
         auto transform = glm::mat4(1.0f);
@@ -82,9 +92,15 @@ TEST(SDLEngineTest, Init)
         transform = glm::rotate(transform, rotationAngle, glm::vec3(0.0f, 0.0f, 1.0f));
         transform = transform * aspectMatrix;
 
-        // Get matrix's uniform location and set matrix
-        glUniformMatrix4fv(transformLoc, 1, GL_FALSE,
-                           glm::value_ptr(transform));
+        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
+
+        glm::vec4 aabbMin = transform * glm::vec4(-0.5f, -0.5f, 0.0f, 1.0f);
+        glm::vec4 aabbMax = transform * glm::vec4(0.5f, 0.5f, 0.0f, 1.0f);
+
+        if (aabbMin.x < -1.0f || aabbMin.y < -1.0f || aabbMax.x > 1.0f || aabbMax.y > 1.0f)
+        {
+            // Handle collision with screen borders.
+        }
 
         glClear(GL_COLOR_BUFFER_BIT);
 
